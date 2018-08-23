f4e829d830c1ea30a761bc2086f0d4bfa410321f
Matt Cool <matthewbcool@gmail.com>
2018-08-22 20:31:07 -0500

    added map component

--- src/App.js
+++ src/App.js
@@ -1,17 +1,12 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import LoganMap from './Components/LoganMap';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <LoganMap />
       </div>
     );
   }
---
+++ src/Components/LoganMap.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import '../App.css'
+
+class LoganMap extends React.Component {
+    state = {
+
+    }
+    render() {
+        return (
+            <div className="map-box">
+            
+            </div>
+        )
+    }
+}
+
+export default LoganMap
